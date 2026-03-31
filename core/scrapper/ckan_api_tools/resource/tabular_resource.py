845d1268a4e6030f392b20fca8ec458cee65d012
h-pgy <hpougy@gmail.com>
2026-03-30 23:31:19 -0300

    automatic encoding solver

--- core/scrapper/ckan_api_tools/resource/tabular_resource.py
+++ core/scrapper/ckan_api_tools/resource/tabular_resource.py
@@ -1,6 +1,7 @@
 from .base_resource import CkanResource
 import pandas as pd
 import os
+from core.utils.download import detect_file_encoding
 from typing import Optional, Any
 
 class TabularResource(CkanResource):
@@ -32,9 +33,13 @@ class TabularResource(CkanResource):
     def fpath(self) -> str:
         return os.path.join(self.folder_path, self.file_name)
     
+    def __solve_encoding(self) -> str:
+        return self.read_kwargs.pop("encoding", None) or detect_file_encoding(self.fpath)
+    
     def __read_tabular_data(self) -> pd.DataFrame:
         if self.format == "csv":
-            return pd.read_csv(self.fpath, **self.read_kwargs)
+            encoding = self.__solve_encoding()
+            return pd.read_csv(self.fpath, encoding=encoding, **self.read_kwargs)
         return pd.read_excel(self.fpath, **self.read_kwargs)
     
     @property
--- core/utils/download.py
+++ core/utils/download.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import chardet
+
 
 def stream_download(url: str, full_path: str, chunk_size: int = 8192) -> str:
     with requests.get(url, stream=True) as r:
@@ -16,3 +18,20 @@ def stream_download(url: str, full_path: str, chunk_size: int = 8192) -> str:
         raise OSError(f"O caminho {full_path} existe, mas não é um arquivo válido.")
     
     return os.path.abspath(full_path)
+
+
+def detect_file_encoding(file_path: str, sample_size: int = 10000) -> str:
+    with open(file_path, 'rb') as f:
+        raw_data = f.read(sample_size)
+        
+        if not raw_data:
+            return "utf-8"
+            
+        result: dict[str, Any] = chardet.detect(raw_data)
+        encoding: str = result.get("encoding", "utf-8")
+        confidence: float = result.get("confidence", 0.0)
+        
+        if encoding is None or confidence < 0.5:
+            return "utf-8"
+            
+        return encoding.lower()
